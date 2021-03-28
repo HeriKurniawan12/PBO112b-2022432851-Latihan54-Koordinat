b05b914e004e980698b0f1ec56db0c263866d7d5
unknown <heri.heri@raharja.info>
2021-03-28 16:57:10 +0700

    Project Latihan 54 koordinat

---
+++ src/pbo112b/pkg2022432851/latihan54/koordinat/PBO112b2022432851Latihan54Koordinat.java
@@ -0,0 +1,26 @@
+
+
+/**
+ *
+ * @author
+ * Nama : Heri Kurniawan
+ * Kelas : PBO112B
+ * NIM : 2022432851
+ * Deskripsi Program : Program Ini Berisi Latihan 54 Koordinat
+ */
+
+package pbo112b.pkg2022432851.latihan54.koordinat;
+
+
+public class PBO112b2022432851Latihan54Koordinat {
+
+
+    public static void main(String[] args) {
+        
+        WarnaKoordinat hki = new WarnaKoordinat(10,4,"Jingga");
+        System.out.println("Warna koordinat : "+hki.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : "+hki.getX()+", y : "+hki.getY());
+
+    }
+    
+}
